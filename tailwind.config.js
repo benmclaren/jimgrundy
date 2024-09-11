6c302bf4d5d25a144459d9caa1ca6eb6c8c579a7
Ben McLaren <benmclaren@live.co.uk>
2024-09-11 13:08:50 +0100

    added tailwind congig

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    `themes/jg/layouts/**/*.html`,
+    `content/**/*.md`
+  ],
+  theme: {
+    extend: {
+      backgroundColor: theme => ({
+
+        ...theme('colors'),
+
+        'highlight': '#F6F6F6',
+        'background': '#F5FDFC'
+      })
+    },
+  },
+  variants: {},
+  plugins: []
+}
